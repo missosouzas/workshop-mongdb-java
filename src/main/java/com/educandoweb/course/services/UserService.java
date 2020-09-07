68f328dacc2795c26f850876e3c4c11e06d98ba7
Edmilson Souza <missosouzas@hotmail.com>
2020-09-07 18:11:31 -0400

    insercao de usuario com POST

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -7,25 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.dto.UserDTO;
 import com.educandoweb.course.repository.UserRepository;
 import com.educandoweb.course.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
-				
-		
+
 	}
+	
+	
+    public User insert(User obj) {
+    	return repo.insert(obj);
+    }
+    
+    public User fromDTO(UserDTO objDTO) {
+    	return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 
 }
