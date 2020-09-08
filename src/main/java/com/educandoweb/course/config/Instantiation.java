d2e844782fb0e041490681b06492dd6e6676ccb7
Edmilson Souza <missosouzas@hotmail.com>
2020-09-07 22:58:23 -0400

    Projecao de Dados do Author com DTO

--- src/main/java/com/educandoweb/course/config/Instantiation.java
+++ src/main/java/com/educandoweb/course/config/Instantiation.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.educandoweb.course.domain.Post;
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.dto.AuthorDTO;
 import com.educandoweb.course.repository.PostRepository;
 import com.educandoweb.course.repository.UserRepository;
 
@@ -38,11 +39,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 	   
-				
-		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo. Abraços",maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"),"Bom dia","Acordei Alegre!",maria);
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+				
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo. Abraços",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"),"Bom dia","Acordei Alegre!",new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		
--- src/main/java/com/educandoweb/course/domain/Post.java
+++ src/main/java/com/educandoweb/course/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.educandoweb.course.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 	
@@ -19,13 +21,13 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO author;
 
 	public Post() {
 
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
@@ -34,11 +36,11 @@ public class Post implements Serializable{
 		this.author=author;
 	}
 
-	public User getAuthor() {
+	public AuthorDTO getAuthor() {
 		return author;
 	}
 
-	public void setAuthor(User author) {
+	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
 
