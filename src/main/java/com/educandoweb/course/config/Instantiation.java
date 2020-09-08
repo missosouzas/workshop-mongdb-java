f00a1759cd80535cca6f2a3f0dbb1515c74dd430
Edmilson Souza <missosouzas@hotmail.com>
2020-09-07 22:30:24 -0400

    Criando Entity Post com User Aninhado

--- src/main/java/com/educandoweb/course/config/Instantiation.java
+++ src/main/java/com/educandoweb/course/config/Instantiation.java
@@ -2,13 +2,17 @@ package com.educandoweb.course.config;
 
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.educandoweb.course.domain.Post;
 import com.educandoweb.course.domain.User;
+import com.educandoweb.course.repository.PostRepository;
 import com.educandoweb.course.repository.UserRepository;
 
 @Configuration
@@ -17,17 +21,30 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
-
+	@Autowired
+	private PostRepository postRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 	
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 	   
+				
+		Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem","Vou viajar para São Paulo. Abraços",maria);
+		Post post2 = new Post(null, sdf.parse("21/03/2018"),"Bom dia","Acordei Alegre!",maria);
+		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		postRepository.saveAll(Arrays.asList(post1,post2));
+		
 		
 		
 	}
